546eb3902bdba877e6ff00496ede38c48cf91d04
kangseohyeon <kshyeon123@gmail.com>
2025-12-01 15:21:28 +0900

    CI eslint 에러 수정

--- packages/eslint-config/node.js
+++ packages/eslint-config/node.js
@@ -2,17 +2,17 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default [
+export default tseslint.config(
   {
     ignores: ["dist", "node_modules", ".turbo"],
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{ts,js}"],
-    ...js.configs.recommended,
-    ...tseslint.configs.recommended,
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.node,
     },
-  },
-];
+  }
+);
