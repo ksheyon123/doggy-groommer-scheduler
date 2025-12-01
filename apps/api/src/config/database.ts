54b9c87700ec1e30c52b94ee02643cb42f0b0d99
kangseohyeon <kshyeon123@gmail.com>
2025-12-02 08:21:17 +0900

    DB 접속 정보 에러 수정

--- apps/api/src/config/database.ts
+++ apps/api/src/config/database.ts
@@ -14,7 +14,7 @@ dotenv.config();
 const sequelize = new Sequelize({
   database: process.env.DB_NAME,
   dialect: "mysql",
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "3306", 10),
--- apps/api/src/scripts/create-db.ts
+++ apps/api/src/scripts/create-db.ts
@@ -1,16 +1,16 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const createDB = async () => {
-  const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+  const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
   try {
     const connection = await mysql.createConnection({
       host: DB_HOST,
-      port: parseInt(DB_PORT || '3306', 10),
-      user: DB_USERNAME,
+      port: parseInt(DB_PORT || "3306", 10),
+      user: DB_USER,
       password: DB_PASSWORD,
     });
 
@@ -18,7 +18,7 @@ const createDB = async () => {
     console.log(`Database ${DB_NAME} created or already exists.`);
     await connection.end();
   } catch (error) {
-    console.error('Error creating database:', error);
+    console.error("Error creating database:", error);
     process.exit(1);
   }
 };
